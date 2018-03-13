7746c429d6947515209fada52daf36e6408d7fea
Markus Shepherd <markus@idtmessaging.com>
2018-03-13 17:11:52 +0200

    added JsonLoader; added actual scraping to WikidataSpider

--- ludoj/loaders.py
+++ ludoj/loaders.py
@@ -2,11 +2,58 @@
 
 ''' Scrapy item loaders '''
 
+import json
+
+import jmespath
+
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import TakeFirst, MapCompose
+from scrapy.utils.misc import arg_to_iter
+from scrapy.utils.python import flatten
 from w3lib.html import remove_tags, replace_entities
 
 from .utils import identity, normalize_space
+
+
+class JsonLoader(ItemLoader):
+    ''' enhance ItemLoader with JMESPath capabilities '''
+
+    def __init__(self, response=None, json_obj=None, **kwargs):
+        super(JsonLoader, self).__init__(response=response, **kwargs)
+
+        if json_obj is None and response is not None:
+            try:
+                json_obj = json.loads(response.text)
+            except Exception:
+                json_obj = None
+
+        self.json_obj = json_obj
+        self.context.setdefault('json', json_obj)
+
+    def _get_jmes_values(self, jmes_paths):
+        if self.json_obj is None:
+            raise RuntimeError('no JSON object found')
+
+        jmes_paths = arg_to_iter(jmes_paths)
+        return flatten(jmespath.search(jmes_path, self.json_obj) for jmes_path in jmes_paths)
+
+    def add_jmes(self, field_name, jmes, *processors, **kw):
+        ''' add values through JMESPath '''
+
+        values = self._get_jmes_values(jmes)
+        self.add_value(field_name, values, *processors, **kw)
+
+    def replace_jmes(self, field_name, jmes, *processors, **kw):
+        ''' replace values through JMESPath '''
+
+        values = self._get_jmes_values(jmes)
+        self.replace_value(field_name, values, *processors, **kw)
+
+    def get_jmes(self, jmes, *processors, **kw):
+        ''' get values through JMESPath '''
+
+        values = self._get_jmes_values(jmes)
+        return self.get_value(values, *processors, **kw)
 
 
 class GameLoader(ItemLoader):
@@ -15,6 +62,11 @@ class GameLoader(ItemLoader):
     default_input_processor = MapCompose(
         identity, str, remove_tags, replace_entities, replace_entities, normalize_space)
     default_output_processor = TakeFirst()
+
+
+class GameJsonLoader(JsonLoader, GameLoader):
+    ''' loader for GameItem plus JMESPath capabilities '''
+    pass
 
 
 class RatingLoader(ItemLoader):
--- ludoj/spiders/wikidata.py
+++ ludoj/spiders/wikidata.py
@@ -2,12 +2,15 @@
 
 ''' Wikidata spider '''
 
+import json
+
 from urllib.parse import urlencode
 
 from scrapy import Request, Spider
 
 from ..items import GameItem
-from ..utils import batchify
+from ..loaders import GameJsonLoader
+from ..utils import batchify, normalize_space
 
 
 class WikidataSpider(Spider):
@@ -19,6 +22,37 @@ class WikidataSpider(Spider):
 
     sparql_api_url = 'https://query.wikidata.org/sparql'
     entity_data_url = 'https://www.wikidata.org/wiki/Special:EntityData/{wikidata_id}.{fformat}'
+
+    game_types = (
+        'Q131436', # board game
+        'Q11410', # game
+        'Q142714', # card game
+        'Q573573', # abstract strategy game
+        'Q839864', # party game
+        'Q734698', # collectible card game
+        'Q788553', # German-style board game
+        'Q1191150', # trick-taking game
+        'Q1272194', # tile-based game
+        'Q1150710', # strategy game
+        'Q532716', # miniature wargaming
+        'Q1368898', # game of skill
+        'Q1509934', # children's game
+        'Q1311927', # railway game
+        'Q1515156', # dice game
+        'Q3244175', # tabletop game
+        'Q4927217', # block wargame
+        'Q2347716', # paper-and-pencil game
+        'Q6983400', # Nazi board games
+        'Q5161688', # connection game
+        'Q1783817', # cooperative board game
+        'Q10927576', # board wargame
+        'Q15220419', # word game
+        'Q14947863', # parlour game
+        'Q5249796', # dedicated deck card game
+        'Q15804899', # Deck-building game
+        'Q21608615', # economic simulation board game
+        'Q28807042', # game variant
+    )
 
     custom_settings = {
         'ROBOTSTXT_OBEY': False,
@@ -34,28 +68,14 @@ class WikidataSpider(Spider):
     def _entity_url(self, wikidata_id, fformat='json'):
         return self.entity_data_url.format(wikidata_id=wikidata_id, fformat=fformat)
 
-    def start_requests(self):
-        ''' generate start requests '''
+    def _type_requests(self, types):
+        query_tmpl = normalize_space('''
+            SELECT DISTINCT ?game WHERE {{
+                ?game <http://www.wikidata.org/prop/direct/P31> ?type .
+                VALUES ?type {{ {} }}
+            }}''')
 
-        query = 'SELECT DISTINCT ?type WHERE { ?game wdt:P2339 ?bgg ; wdt:P31 ?type . }'
-        self.logger.debug(query)
-        yield Request(self._api_url(query), callback=self.parse)
-
-    def parse(self, response):
-        ''' TODO contract '''
-
-        response.selector.register_namespace('sparql', 'http://www.w3.org/2005/sparql-results#')
-
-        # self.logger.info(response.text[:500])
-
-        types = response.xpath('//sparql:binding[@name = "type"]/sparql:uri/text()').extract()
-
-        self.logger.info('received %d types', len(types))
-
-        query_tmpl = (
-            'SELECT DISTINCT ?game WHERE {{ ?game wdt:P31 ?type . VALUES ?type {{ {} }} }}')
-
-        # query = query_tmpl.format(' '.join('<{}>'.format(x) for x in types))
+        # query = query_tmpl.format(' '.join(types))
         # return Request(
         #     self.sparql_api_url,
         #     method='POST',
@@ -63,16 +83,42 @@ class WikidataSpider(Spider):
         #     callback=self.parse_games)
 
         for batch in batchify(types, 10):
-            query = query_tmpl.format(' '.join('<{}>'.format(x) for x in batch))
+            query = query_tmpl.format(' '.join(batch))
             self.logger.debug(query)
             yield Request(self._api_url(query), callback=self.parse_games)
 
-    def parse_games(self, response):
-        ''' TODO contract '''
+    def start_requests(self):
+        ''' generate start requests '''
+
+        types = getattr(self, 'game_types', None)
+
+        if types:
+            yield from self._type_requests(
+                '<http://www.wikidata.org/entity/{}>'.format(t) for t in types)
+            return
+
+        query = normalize_space('''
+            SELECT DISTINCT ?type WHERE {
+                ?game <http://www.wikidata.org/prop/direct/P2339> ?bgg ;
+                      <http://www.wikidata.org/prop/direct/P31> ?type .
+            }''')
+        self.logger.debug(query)
+        yield Request(self._api_url(query), callback=self.parse)
+
+    # TODO contract
+    def parse(self, response):
+        ''' contract '''
 
         response.selector.register_namespace('sparql', 'http://www.w3.org/2005/sparql-results#')
+        types = response.xpath('//sparql:binding[@name = "type"]/sparql:uri/text()').extract()
+        self.logger.info('received %d types', len(types))
+        yield from self._type_requests('<{}>'.format(t) for t in types)
 
-        # self.logger.info(response.text[:500])
+    # TODO contract
+    def parse_games(self, response):
+        ''' contract '''
+
+        response.selector.register_namespace('sparql', 'http://www.w3.org/2005/sparql-results#')
 
         games = response.xpath('//sparql:binding[@name = "game"]/sparql:uri/text()').extract()
 
@@ -81,6 +127,53 @@ class WikidataSpider(Spider):
         for game in games:
             # TODO make more robust
             wikidata_id = game.split('/')[-1]
-            # self.logger.info('found ID <%s>', wikidata_id)
-            self.logger.info(self._entity_url(wikidata_id))
-            # yield Request(self._entity_url(wikidata_id), callback=self.parse_game)
+            yield Request(self._entity_url(wikidata_id), callback=self.parse_game)
+
+    def parse_game(self, response):
+        '''
+        @url https://www.wikidata.org/wiki/Special:EntityData/Q17271.json
+        @returns items 1 1
+        @returns requests 0 0
+        @scrapes name alt_name designer publisher url image_url external_link year \
+            min_players max_players bgg_id wikidata_id freebase_id
+        '''
+
+        try:
+            result = json.loads(response.text)
+        except Exception as exc:
+            self.logger.warning(exc)
+            return
+
+        for game in result.get('entities', {}).values():
+            ldr = GameJsonLoader(item=GameItem(), json_obj=game, response=response)
+
+            ldr.add_jmes('name', 'labels.en.value')
+            ldr.add_jmes('name', 'aliases.en[].value')
+            ldr.add_jmes('name', 'labels.*.value')
+            ldr.add_jmes('name', 'aliases.*[].value')
+            ldr.add_jmes('alt_name', 'labels.*.value')
+            ldr.add_jmes('alt_name', 'aliases.*[].value')
+            # TODO parse time to year
+            ldr.add_jmes('year', 'claims.P577[].mainsnak.datavalue.value.time')
+
+            # TODO only ID, need to fetch label
+            ldr.add_jmes('designer', 'claims.P178[].mainsnak.datavalue.value.id')
+            ldr.add_jmes('publisher', 'claims.P123[].mainsnak.datavalue.value.id')
+
+            ldr.add_value('url', response.url)
+            # TODO only file name, expand to URL
+            ldr.add_jmes('image_url', 'claims.P18[].mainsnak.datavalue.value')
+            # official website
+            ldr.add_jmes('external_link', 'claims.P856[].mainsnak.datavalue.value')
+            # Wikipedia pages
+            ldr.add_jmes('external_link', 'sitelinks.*.url')
+
+            ldr.add_jmes('min_players', 'claims.P1872[].mainsnak.datavalue.value.amount')
+            ldr.add_jmes('max_players', 'claims.P1873[].mainsnak.datavalue.value.amount')
+
+            ldr.add_jmes('bgg_id', 'claims.P2339[].mainsnak.datavalue.value')
+            ldr.add_jmes('wikidata_id', 'id')
+            ldr.add_jmes('wikidata_id', 'title')
+            ldr.add_jmes('freebase_id', 'claims.P646[].mainsnak.datavalue.value')
+
+            yield ldr.load_item()
