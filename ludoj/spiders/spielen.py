ccfc52b6e7c7691e15e434270c4d6b6a5935a490
Markus Shepherd <markus@idtmessaging.com>
2018-04-19 14:04:42 +0300

    updated SpielenSpider to adjust for changes in website, resolving #5

--- ludoj/spiders/spielen.py
+++ ludoj/spiders/spielen.py
@@ -4,7 +4,7 @@
 
 import re
 
-from scrapy import Request, Spider
+from scrapy import Spider
 
 from ..items import GameItem
 from ..loaders import GameLoader
@@ -42,12 +42,12 @@ class SpielenSpider(Spider):
         next_page = (response.css('.listPagination a.glyphicon-step-forward::attr(href)')
                      .extract_first())
         if next_page:
-            yield Request(response.urljoin(next_page), callback=self.parse)
+            yield response.follow(next_page, callback=self.parse)
 
         for game in response.css('div.listItem'):
             url = game.css('h3 a::attr(href)').extract_first()
             if url:
-                yield Request(response.urljoin(url), callback=self.parse_game)
+                yield response.follow(url, callback=self.parse_game)
 
     # pylint: disable=no-self-use
     def parse_game(self, response):
@@ -57,57 +57,73 @@ class SpielenSpider(Spider):
         @returns requests 0 0
         @scrapes name year description designer artist publisher \
                  url image_url video_url \
-                 min_players max_players min_age min_time max_time \
+                 min_players max_players min_age min_time max_time family \
                  num_votes avg_rating worst_rating best_rating \
                  complexity easiest_complexity hardest_complexity
         '''
 
         game = response.css('div.fullBox')
 
-        ldr = GameLoader(item=GameItem(), selector=game, response=response)
+        ldr = GameLoader(
+            item=GameItem(
+                worst_rating=1,
+                best_rating=5,
+                easiest_complexity=1,
+                hardest_complexity=5,
+            ),
+            selector=game,
+            response=response,
+        )
 
         ldr.add_css('name', 'h2')
-        ldr.add_xpath('year', './/b[. = "Erscheinungsjahr:"]/following-sibling::text()[1]')
+        ldr.add_xpath('year', './/div[b = "Erscheinungsjahr:"]/following-sibling::div//text()')
         ldr.add_xpath('description', './/h2/following-sibling::text()')
 
-        ldr.add_xpath('designer', './/text()[(preceding-sibling::b/text() = "Autor:" '
-                                  'or preceding-sibling::b = "Autoren:") '
-                                  'and (following-sibling::b = "Illustrator:" '
-                                  'or following-sibling::b = "Illustratoren:")]')
-        ldr.add_xpath('artist', './/text()[(preceding-sibling::b = "Illustrator:" '
-                                'or preceding-sibling::b = "Illustratoren:") '
-                                'and following-sibling::b = "Erscheinungsjahr:"]')
-        ldr.add_xpath('publisher', './/b[. = "Verlag:" or . = "Verlage:"]/following-sibling::a')
+        ldr.add_xpath(
+            'designer', './/div[b = "Autor:" or b = "Autoren:"]/following-sibling::div//text()')
+        ldr.add_xpath(
+            'artist',
+            './/div[b = "Illustrator:" or b = "Illustratoren:"]/following-sibling::div//text()')
+        ldr.add_xpath(
+            'publisher', './/div[b = "Verlag:" or b = "Verlage:"]/following-sibling::div//a')
 
         ldr.add_value('url', response.url)
-        images = ([game.xpath('(.//img)[1]/@data-src').extract_first(),
-                   game.xpath('(.//a[img])[1]/@href').extract_first()]
-                  + game.css('div.screenshotlist img::attr(data-large-src)').extract())
+
+        images = [
+            game.xpath('(.//img)[1]/@data-src').extract_first(),
+            game.xpath('(.//a[img])[1]/@href').extract_first(),
+        ] + game.css('div.screenshotlist img::attr(data-large-src)').extract()
         ldr.add_value('image_url', (response.urljoin(i) for i in images if i))
+
         videos = game.css('iframe::attr(src)').extract()
         ldr.add_value('video_url', (response.urljoin(v) for v in videos if v))
 
-        players = game.xpath('.//b[. = "Spieler:"]/following-sibling::text()').extract_first()
+        players = game.xpath(
+            './/div[b = "Spieler:"]/following-sibling::div/text()').extract_first()
+        # TODO parse 'besonders gut mit 4 Spielern'
         min_players, max_players = _parse_interval(players) if players else (None, None)
         ldr.add_value('min_players', min_players)
         ldr.add_value('max_players', max_players)
-        age = game.xpath('.//b[. = "Alter:"]/following-sibling::text()').extract_first()
+
+        age = game.xpath('.//div[b = "Alter:"]/following-sibling::div/text()').extract_first()
         ldr.add_value('min_age', _parse_int(age) if age else None)
-        time = game.xpath('.//b[. = "Dauer:"]/following-sibling::text()').extract_first()
+
+        time = game.xpath('.//div[b = "Dauer:"]/following-sibling::div/text()').extract_first()
         min_time, max_time = _parse_interval(time) if time else (None, None)
         ldr.add_value('min_time', min_time)
         ldr.add_value('max_time', max_time)
 
+        ldr.add_xpath(
+            'family',
+            './/div[b = "Spielfamilie:" or b = "Spielfamilien:"]/following-sibling::div//text()')
+
         ldr.add_css('num_votes', 'span.votes')
         ldr.add_css('avg_rating', 'span.average')
-        ldr.add_value('worst_rating', '1')
-        ldr.add_value('best_rating', '5')
 
-        complexity = game.xpath('.//div[. = "Komplexität:"]/following-sibling::div'
-                                '/span[following-sibling::span[contains(@class, "red")]]')
+        complexity = game.xpath(
+            './/div[. = "Komplexität:"]/following-sibling::div'
+            '/span[following-sibling::span[contains(@class, "red")]]')
         complexity = len(complexity) + 1
-        ldr.add_value('complexity', str(complexity))
-        ldr.add_value('easiest_complexity', '1')
-        ldr.add_value('hardest_complexity', '5')
+        ldr.add_value('complexity', complexity)
 
         return ldr.load_item()
