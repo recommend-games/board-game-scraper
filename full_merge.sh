fe5cfd47d9c4397bc024905716198d959c77e4f2
Markus Shepherd <markus@idtmessaging.com>
2018-09-11 14:06:56 +0300

    better merge scripts

--- full_merge.sh
+++ full_merge.sh
@@ -4,93 +4,64 @@ set -euo pipefail
 
 # rsync -av -e 'ssh -p 2222' monkeybear:~/Workspace/ludoj-scraper/feeds/ feeds/
 
+DATE="$(date --utc +'%Y-%m-%dT%H-%M-%S')_merged"
+
 nohup python3 -m ludoj.merge \
     'feeds/bgg/GameItem/' \
-    --out-file 'results/bgg.csv' \
+    --out-file "feeds/bgg/GameItem/$DATE.csv" \
     --keys bgg_id \
     --key-types int \
     --latest scraped_at \
     --latest-type date \
-    --fields-exclude game_type list_price \
-        freebase_id wikidata_id wikipedia_id dbpedia_id luding_id \
-        published_at updated_at scraped_at \
     >> 'logs/bgg_merge.log' 2>&1 &
 echo -e "Started! Follow logs from <$(pwd)/logs/bgg_merge.log>.\\n"
 
 nohup python3 -m ludoj.merge \
     'feeds/dbpedia/GameItem/' \
-    --out-file 'results/dbpedia.csv' \
+    --out-file "feeds/dbpedia/GameItem/$DATE.csv" \
     --keys dbpedia_id \
     --key-types string \
     --latest scraped_at \
     --latest-type date \
-    --fields name alt_name year \
-        description designer publisher \
-        url image_url external_link \
-        min_players max_players \
-        min_age max_age \
-        bgg_id dbpedia_id \
     >> 'logs/dbpedia_merge.log' 2>&1 &
 echo -e "Started! Follow logs from <$(pwd)/logs/dbpedia_merge.log>.\\n"
 
 nohup python3 -m ludoj.merge \
     'feeds/luding/GameItem/' \
-    --out-file 'results/luding.csv' \
+    --out-file "feeds/luding/GameItem/$DATE.csv" \
     --keys luding_id \
     --key-types int \
     --latest scraped_at \
     --latest-type date \
-    --fields name year game_type \
-        description designer artist publisher \
-        url image_url external_link \
-        min_players max_players \
-        min_age max_age \
-        bgg_id luding_id \
     >> 'logs/luding_merge.log' 2>&1 &
 echo -e "Started! Follow logs from <$(pwd)/logs/luding_merge.log>.\\n"
 
 nohup python3 -m ludoj.merge \
     'feeds/spielen/GameItem/' \
-    --out-file 'results/spielen.csv' \
+    --out-file "feeds/spielen/GameItem/$DATE.csv" \
     --keys url \
     --key-types string \
     --latest scraped_at \
     --latest-type date \
-    --fields name year description \
-        designer artist publisher \
-        url image_url video_url \
-        min_players max_players \
-        min_age max_age \
-        min_time max_time family \
-        num_votes avg_rating worst_rating best_rating \
-        complexity easiest_complexity hardest_complexity \
     >> 'logs/spielen_merge.log' 2>&1 &
 echo -e "Started! Follow logs from <$(pwd)/logs/spielen_merge.log>.\\n"
 
 nohup python3 -m ludoj.merge \
     'feeds/wikidata/GameItem/' \
-    --out-file 'results/wikidata.csv' \
+    --out-file "feeds/wikidata/GameItem/$DATE.csv" \
     --keys wikidata_id \
     --key-types string \
     --latest scraped_at \
     --latest-type date \
-    --fields name alt_name year \
-        designer publisher \
-        url image_url external_link \
-        min_players max_players \
-        min_age max_age \
-        min_time max_time family \
-        bgg_id freebase_id wikidata_id luding_id \
     >> 'logs/wikidata_merge.log' 2>&1 &
 echo -e "Started! Follow logs from <$(pwd)/logs/wikidata_merge.log>.\\n"
 
 nohup python3 -m ludoj.merge \
     'feeds/bgg/RatingItem/' \
-    --out-file 'results/bgg_ratings.csv' \
+    --out-file "feeds/bgg/RatingItem/$DATE.csv" \
     --keys bgg_id bgg_user_name \
     --key-types int string \
     --latest scraped_at \
     --latest-type date \
-    --fields bgg_id bgg_user_name bgg_user_rating \
     >> 'logs/bgg_ratings_merge.log' 2>&1 &
 echo -e "Started! Follow logs from <$(pwd)/logs/bgg_ratings_merge.log>.\\n"
