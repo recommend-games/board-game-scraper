502e5189bb31d7201f24f80fd3654b3f0b11a57c
Markus Shepherd <markus@idtmessaging.com>
2018-10-24 15:00:53 +0300

    added upload functionality to ludoj.json; migrated from requirements.txt to Pipfile

--- ludoj/json.py
+++ ludoj/json.py
@@ -4,39 +4,190 @@
 ''' JSON parsing '''
 
 import argparse
+import json
 import logging
 import os.path
 import sys
 
 from smart_open import smart_open
 
+import requests
+
 from .items import GameItem
 from .utils import serialize_json
 
 LOGGER = logging.getLogger(__name__)
 
+FIELDS = frozenset((
+    'avg_rating',
+    'bayes_rating',
+    'bgg_id',
+    'bgg_rank',
+    'compilation',
+    'complexity',
+    'cooperative',
+    'created_at',
+    'description',
+    'language_dependency',
+    'max_age',
+    'max_age_rec',
+    'max_players',
+    'max_players_best',
+    'max_players_rec',
+    'max_time',
+    'min_age',
+    'min_age_rec',
+    'min_players',
+    'min_players_best',
+    'min_players_rec',
+    'min_time',
+    'modified_at',
+    'name',
+    'num_votes',
+    'rec_rank',
+    'rec_rating',
+    'scraped_at',
+    'stddev_rating',
+    'url',
+    'year',
+))
 
-def _write_jsonl(items, output=sys.stdout, **kwargs):
+FIELDS_MAPPING = {
+    'rank': 'bgg_rank',
+    # 'implementation': 'implementation_of',
+}
+
+
+def _parse_item(item, fields=FIELDS, fields_mapping=None):
+    result = {k: v for k, v in item.items() if k in fields}
+
+    if not fields_mapping:
+        return result
+
+    for map_from, map_to in fields_mapping.items():
+        if item.get(map_from):
+            result.setdefault(map_to, item[map_from])
+
+    return result
+
+
+def _upload(items, url, id_field='bgg_id', fields=FIELDS, fields_mapping=None):
+    LOGGER.info('uploading items to <%s>', url)
+
+    count = 0
+
+    for item in items:
+        data = _parse_item(item, fields, fields_mapping)
+        id_ = data.get(id_field)
+        if id_ is None:
+            LOGGER.warning('no ID found in %r', data)
+            continue
+
+        url_item = os.path.join(url, str(id_), '')
+        response = requests.head(url_item)
+        response = (
+            requests.put(url=url_item, data=data) if response.ok
+            else requests.post(url=url, data=data))
+
+        if response.ok:
+            count += 1
+            if count % 1000 == 0:
+                LOGGER.info('uploaded %d items so far', count)
+        else:
+            LOGGER.warning(
+                'there was a problem with the request for %r; reason: %s', data, response.reason)
+
+
+        yield item
+
+
+def _format_from_path(path):
+    try:
+        _, ext = os.path.splitext(path)
+    except Exception:
+        return None
+    return ext.lower()[1:] if ext else None
+
+
+def _load_json(path):
+    LOGGER.info('loading JSON from <%s>...', path)
+    with smart_open(path, 'r') as json_file:
+        yield from json.load(json_file)
+
+
+def _load_jl(path):
+    LOGGER.info('loading JSON lines from <%s>...', path)
+    with smart_open(path, 'r') as json_file:
+        for line in json_file:
+            yield json.loads(line)
+
+
+def _load(*paths):
+    for path in paths:
+        in_format = _format_from_path(path)
+
+        if in_format in ('jl', 'jsonl'):
+            yield from _load_jl(path)
+        elif in_format == 'json':
+            yield from _load_json(path)
+        else:
+            yield from GameItem.from_csv(path)
+
+
+def _write_json(items, output=sys.stdout, jsonl=False, **kwargs):
     if isinstance(output, (str, bytes)):
         with smart_open(output, 'w') as output_file:
-            return _write_jsonl(items=items, output=output_file, **kwargs)
+            return _write_json(items=items, output=output_file, jsonl=jsonl, **kwargs)
+
+    if jsonl:
+        kwargs['indent'] = None
+    else:
+        output.write('[\n')
 
     count = -1
 
     for count, item in enumerate(items):
+        if count:
+            if count % 1000 == 0:
+                LOGGER.info('wrote %d items so far', count)
+            output.write('\n' if jsonl else ',\n')
         serialize_json(item, file=output, **kwargs)
-        output.write('\n')
+
+    output.write('\n' if jsonl else '\n]\n')
 
     return count + 1
 
 
+def _write(items, out_path=None, out_format=None, **json_kwargs):
+    out_format = out_format or _format_from_path(out_path)
+    jsonl = out_format in ('jsonl', 'jl')
+    # games = GameItem.from_csv(*csv_paths)
+
+    output = sys.stdout if not out_path or out_path == '-' else out_path
+    LOGGER.info('output location for game items: %r', output)
+    LOGGER.info('output format: %s', 'JSON Lines' if jsonl else 'JSON')
+
+    count = _write_json(
+        items=items,
+        output=output,
+        jsonl=jsonl,
+        **json_kwargs,
+    )
+
+    LOGGER.info('done writing %d items', count)
+
+    return count
+
+
 def _parse_args():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('paths', nargs='*', help='')
+    parser.add_argument('paths', nargs='+', help='')
+    parser.add_argument('--url', '-u', help='')
+    parser.add_argument('--id-field', '-i', default='bgg_id', help='')
     parser.add_argument('--output', '-o', help='')
-    parser.add_argument('--format', '-f', choices=('json', 'jsonl', 'jl'), help='')
-    parser.add_argument('--indent', '-i', type=int, help='')
-    parser.add_argument('--sort-keys', '-s', action='store_true', help='')
+    parser.add_argument('--out-format', '-f', choices=('json', 'jsonl', 'jl'), help='')
+    parser.add_argument('--indent', '-I', type=int, help='')
+    parser.add_argument('--sort-keys', '-S', action='store_true', help='')
     parser.add_argument(
         '--verbose', '-v', action='count', default=0, help='log level (repeat for more verbosity)')
 
@@ -52,41 +203,37 @@ def _main():
         format='%(asctime)s %(levelname)-8.8s [%(name)s:%(lineno)s] %(message)s'
     )
 
-    if not args.format and args.output:
-        _, ext = os.path.splitext(args.output)
-        if ext:
-            args.format = ext.lower()[1:]
-
     LOGGER.info(args)
 
-    games = GameItem.from_csv(*args.paths)
+    items = _load(*args.paths)
 
-    output = sys.stdout if not args.output or args.output == '-' else args.output
-    LOGGER.info('output location for game items: %r', output)
+    if args.url:
+        items = _upload(
+            items=items,
+            url=args.url,
+            id_field=args.id_field,
+            fields=FIELDS,
+            fields_mapping=FIELDS_MAPPING,
+        )
 
-    if args.format in ('jsonl', 'jl'):
-        LOGGER.info('output format: JSON Lines')
-        count = _write_jsonl(
-            items=games,
-            output=output,
+    if args.output:
+        count = _write(
+            items=items,
+            out_path=args.output,
+            out_format=args.out_format,
             indent=args.indent,
             sort_keys=args.sort_keys,
         )
 
     else:
-        LOGGER.info('output format: JSON')
-        games = tuple(games)
-        serialize_json(
-            obj=games,
-            file=output,
-            indent=args.indent,
-            sort_keys=args.sort_keys,
-        )
-        count = len(games)
+        count = -1
+        for count, _ in enumerate(items):
+            pass
+        count += 1
 
-    del games
+    del items
 
-    LOGGER.info('done writing %d items', count)
+    LOGGER.info('done processing %d items', count)
 
 
 if __name__ == '__main__':
