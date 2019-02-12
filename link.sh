9e1bce1ebefa9eae31983b67a02f0e6f3f8d9a1d
Markus Shepherd <markus@idtmessaging.com>
2019-02-12 09:29:36 +0200

    added link.sh

---
+++ link.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+python -m ludoj_scraper.cluster \
+    --recall .75 \
+    --output ../ludoj-data/links.json \
+    ../ludoj-data/scraped/bgg.jl \
+    ../ludoj-data/scraped/bga.jl \
+    ../ludoj-data/scraped/spielen.jl \
+    ../ludoj-data/scraped/luding.jl \
+    ../ludoj-data/scraped/wikidata.jl
