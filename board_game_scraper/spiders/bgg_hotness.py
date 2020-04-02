e485d63472831cf3aac47962ea6e74219b70bb8b
Markus Shepherd <markus.schepke@maas.global>
2020-04-02 19:23:21 +0300

    added BggHotnessSpider.parse_dir()

--- board_game_scraper/spiders/bgg_hotness.py
+++ board_game_scraper/spiders/bgg_hotness.py
@@ -2,7 +2,13 @@
 
 """BoardGameGeek hotness spider."""
 
+from datetime import timezone
+from pathlib import Path
+
+from pytility import parse_date
 from scrapy import Spider
+from scrapy.http import XmlResponse
+from scrapy.utils.misc import arg_to_iter
 
 from ..items import GameItem
 from ..loaders import GameLoader
@@ -27,7 +33,7 @@ class BggHotnessSpider(Spider):
         "AUTOTHROTTLE_HTTP_CODES": (429, 503, 504),
     }
 
-    def parse(self, response):
+    def parse(self, response, **values):
         """
         @url https://www.boardgamegeek.com/xmlapi2/hot?type=boardgame
         @returns items 50 50
@@ -35,20 +41,40 @@ class BggHotnessSpider(Spider):
         @scrapes published_at rank bgg_id name year image_url scraped_at
         """
 
-        scraped_at = now()
-        published_at = scraped_at
+        curr_time = now()
 
-        for item in response.xpath("/items/item"):
-            ldr = GameLoader(
-                item=GameItem(published_at=published_at, scraped_at=scraped_at),
-                selector=item,
-                response=response,
-            )
+        for game in response.xpath("/items/item"):
+            ldr = GameLoader(item=GameItem(values), selector=game, response=response)
 
             ldr.add_xpath("bgg_id", "@id")
             ldr.add_xpath("rank", "@rank")
             ldr.add_xpath("name", "name/@value")
             ldr.add_xpath("year", "yearpublished/@value")
             ldr.add_xpath("image_url", "thumbnail/@value")
+            ldr.add_value("published_at", curr_time)
+            ldr.add_value("scraped_at", curr_time)
 
             yield ldr.load_item()
+
+    def parse_dir(self, path="."):
+        """TODO."""
+
+        path = Path(path)
+
+        for file_path in path.iterdir():
+            if not file_path.is_file():
+                continue
+
+            self.logger.info("Processing <%s>", file_path)
+
+            date = parse_date(file_path.stem, tzinfo=timezone.utc)
+
+            with file_path.open("rb") as file_obj:
+                response = XmlResponse(
+                    url="https://www.boardgamegeek.com/xmlapi2/hot?type=boardgame",
+                    body=file_obj.read(),
+                )
+
+            yield from arg_to_iter(
+                self.parse(response=response, published_at=date, scraped_at=date)
+            )
