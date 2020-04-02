2de12c8c8b4ec6d79a5cf63524bd44f27488bd7b
Markus Shepherd <markus.schepke@maas.global>
2020-04-02 22:50:28 +0300

    make local requests

--- board_game_scraper/spiders/bgg_hotness.py
+++ board_game_scraper/spiders/bgg_hotness.py
@@ -6,9 +6,7 @@ from datetime import timezone
 from pathlib import Path
 
 from pytility import parse_date
-from scrapy import Spider
-from scrapy.http import XmlResponse
-from scrapy.utils.misc import arg_to_iter
+from scrapy import Request, Spider
 
 from ..items import GameItem
 from ..loaders import GameLoader
@@ -24,16 +22,26 @@ class BggHotnessSpider(Spider):
     item_classes = (GameItem,)
 
     custom_settings = {
-        "DOWNLOAD_DELAY": 0.5,
-        "CONCURRENT_REQUESTS_PER_DOMAIN": 8,
-        "AUTOTHROTTLE_TARGET_CONCURRENCY": 4,
+        "DOWNLOAD_DELAY": 0,
+        "CONCURRENT_REQUESTS_PER_DOMAIN": 1024,
+        "AUTOTHROTTLE_TARGET_CONCURRENCY": 1024,
         "DELAYED_RETRY_ENABLED": True,
         "DELAYED_RETRY_HTTP_CODES": (202,),
         "DELAYED_RETRY_DELAY": 5.0,
         "AUTOTHROTTLE_HTTP_CODES": (429, 503, 504),
     }
 
-    def parse(self, response, **values):
+    def start_requests(self):
+        hotness_dir = self.settings.get("BGG_HOTNESS_DIR")
+
+        if hotness_dir:
+            self.logger.info("Loading local files from <%s>", hotness_dir)
+            yield from self.local_requests(hotness_dir)
+
+        else:
+            yield from super().start_requests()
+
+    def parse(self, response):
         """
         @url https://www.boardgamegeek.com/xmlapi2/hot?type=boardgame
         @returns items 50 50
@@ -41,40 +49,36 @@ class BggHotnessSpider(Spider):
         @scrapes published_at rank bgg_id name year image_url scraped_at
         """
 
-        curr_time = now()
+        scraped_at = now()
 
         for game in response.xpath("/items/item"):
-            ldr = GameLoader(item=GameItem(values), selector=game, response=response)
+            ldr = GameLoader(item=GameItem(), selector=game, response=response)
 
             ldr.add_xpath("bgg_id", "@id")
             ldr.add_xpath("rank", "@rank")
             ldr.add_xpath("name", "name/@value")
             ldr.add_xpath("year", "yearpublished/@value")
             ldr.add_xpath("image_url", "thumbnail/@value")
-            ldr.add_value("published_at", curr_time)
-            ldr.add_value("scraped_at", curr_time)
+
+            ldr.add_value("published_at", response.meta.get("published_at"))
+            ldr.add_value("published_at", scraped_at)
+            ldr.add_value("scraped_at", scraped_at)
 
             yield ldr.load_item()
 
-    def parse_dir(self, path="."):
+    def local_requests(self, path_dir="."):
         """TODO."""
 
-        path = Path(path)
+        path_dir = Path(path_dir).resolve()
 
-        for file_path in path.iterdir():
-            if not file_path.is_file():
+        for path_file in path_dir.iterdir():
+            if not path_file.is_file():
                 continue
 
-            self.logger.info("Processing <%s>", file_path)
+            self.logger.info("Processing <%s>", path_file)
 
-            date = parse_date(file_path.stem, tzinfo=timezone.utc)
+            date = parse_date(path_file.stem, tzinfo=timezone.utc)
 
-            with file_path.open("rb") as file_obj:
-                response = XmlResponse(
-                    url="https://www.boardgamegeek.com/xmlapi2/hot?type=boardgame",
-                    body=file_obj.read(),
-                )
-
-            yield from arg_to_iter(
-                self.parse(response=response, published_at=date, scraped_at=date)
+            yield Request(
+                url=path_file.as_uri(), callback=self.parse, meta={"published_at": date}
             )
