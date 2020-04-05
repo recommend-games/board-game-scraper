4d52102823ddee0813e90df02ff47f4f22025a7b
Markus Shepherd <markus.schepke@maas.global>
2020-04-05 08:15:42 +0300

    reinstate old run_scrapers.sh (for now)

---
+++ run_scrapers.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SAVEDIR="$(pwd)"
+cd "$(dirname "$(readlink --canonicalize "${BASH_SOURCE[0]}")")"
+
+mkdir --parents 'logs'
+
+if [ $# -eq 0 ]; then
+    SCRAPERS=($(scrapy list))
+else
+    SCRAPERS=("$@")
+fi
+
+for SCRAPER in "${SCRAPERS[@]}"; do
+    nohup bash run_scraper.sh "${SCRAPER}" >> "logs/${SCRAPER}.log" 2>&1 &
+    echo "Follow logs from <$(pwd)/logs/${SCRAPER}.log>"
+done
+
+cd "${SAVEDIR}"
