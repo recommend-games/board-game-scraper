72924f594301b52072a43d2e46bfb9603d36250a
Markus Shepherd <markus.schepke@maas.global>
2020-04-13 09:40:27 +0300

    more work on __main__

--- board_game_scraper/__main__.py
+++ board_game_scraper/__main__.py
@@ -2,30 +2,39 @@
 
 """Command line entry point."""
 
+import argparse
 import logging
+import sys
 
 from pathlib import Path
 
 from pytility import normalize_space
 from scrapy.cmdline import execute
-from scrapy.utils.job import job_dir
+from scrapy.utils.job import job_dir as job_dir_from_settings
 from scrapy.utils.misc import arg_to_iter
+from scrapy.utils.project import get_project_settings
 from scrapy.utils.python import garbage_collect
 
+from .utils import now
+
 LOGGER = logging.getLogger(__name__)
+DATE_FORMAT = "%Y-%m-%dT%H-%M-%S"
+RESUMABLE_STATES = frozenset(("shutdown", "closespider_timeout"))
 
 
-def _find_state(
-    path_dir, state_file=".state", delete=("shutdown", "closespider_timeout")
+def _find_states(
+    path_dir, state_file=".state", delete="finished", delete_non_state=False,
 ):
     path_dir = Path(path_dir).resolve()
     delete = frozenset(arg_to_iter(delete))
     result = {}
 
-    for sub_dir in sorted(path_dir.iterdir(), reverse=True):
-        print(sub_dir)
+    if not path_dir.is_dir():
+        LOGGER.warning("<%s> is not an existing dir", path_dir)
+        return result
+
+    for sub_dir in path_dir.iterdir():
         state_path = sub_dir / state_file
-        print(state_path)
 
         if not sub_dir.is_dir() or not state_path.is_file():
             continue
@@ -37,20 +46,95 @@ def _find_state(
             LOGGER.exeception("Unable to read a state from <%s>", state_path)
             state = None
 
-        if not state:
-            continue
-
-        result[sub_dir.name] = state
-
-        if state in delete:
+        if state in delete or (delete_non_state and not state):
             LOGGER.info("Deleting <%s> with state <%s>", sub_dir, state)
             # TODO delete
+            continue
+
+        if state:
+            result[sub_dir.name] = state
 
     return result
 
 
+def _parse_args():
+    parser = argparse.ArgumentParser(description="TODO")
+    parser.add_argument("spider", help="TODO")
+    parser.add_argument("--job-dir", "-j", help="TODO")
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        action="count",
+        default=0,
+        help="log level (repeat for more verbosity)",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    """Command line entry point."""
+
+    args = _parse_args()
+
+    logging.basicConfig(
+        stream=sys.stderr,
+        level=logging.DEBUG if args.verbose > 0 else logging.INFO,
+        format="%(asctime)s %(levelname)-8.8s [%(name)s:%(lineno)s] %(message)s",
+    )
+
+    LOGGER.info(args)
+
+    settings = get_project_settings()
+
+    base_dir = Path(settings["BASE_DIR"]).resolve()
+    from_settings = job_dir_from_settings(settings)
+    job_dir = (
+        Path(args.job_dir)
+        if args.job_dir
+        else Path(from_settings)
+        if from_settings
+        else base_dir / "jobs" / args.spider
+    )
+    job_dir = job_dir.resolve()
+
+    states = _find_states(job_dir)
+
+    running = sorted(sub_dir for sub_dir, state in states.items() if state == "running")
+
+    if len(running) > 1:
+        LOGGER.warning(
+            "Found %d running jobs %s, please check and fix!", len(running), running
+        )
+        return
+
+    if running:
+        LOGGER.info("Found a running job <%s>, skipping...", running[0])
+        return
+
+    resumable = sorted(
+        sub_dir for sub_dir, state in states.items() if state in RESUMABLE_STATES
+    )
+
+    if len(resumable) > 1:
+        LOGGER.warning(
+            "Found %d resumable jobs %s, please check and fix!",
+            len(resumable),
+            resumable,
+        )
+        return
+
+    if resumable:
+        LOGGER.info("Resuming previous job <%s>", resumable[0])
+
+    job_tag = resumable[0] if resumable else now().strftime(DATE_FORMAT)
+    curr_job = job_dir / job_tag
+
+    # try:
+    #     execute()
+    # finally:
+    #     garbage_collect()
+
+
 if __name__ == "__main__":
-    try:
-        execute()
-    finally:
-        garbage_collect()
+    main()
