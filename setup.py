846b8e7d240948892f40620b21ae3cf595067201
Markus Shepherd <markus.schepke@maas.global>
2020-04-13 17:34:57 +0300

    added command line entry points, resolving #18

--- setup.py
+++ setup.py
@@ -153,9 +153,12 @@ setup(
     packages=find_packages(exclude=("tests", "*.tests", "*.tests.*", "tests.*")),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=(),
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
+    entry_points={
+        "console_scripts": (
+            "bg-scraper=board_game_scraper.__main__:main",
+            "bg-merge=board_game_scraper.merge:main",
+        ),
+    },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
