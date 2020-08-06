8f09a604705c3afcabddf3fa8105a3345cd0b994
Markus Shepherd <markus.schepke@maas.global>
2020-08-06 09:31:35 +0300

    skeleton

---
+++ board_game_scraper/full_merge.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Stop scraper, merge files, delete old files, and resume scraping."""
+
+
+def main():
+    """Command line entry point."""
+
+
+if __name__ == "__main__":
+    main()
