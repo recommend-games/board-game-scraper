2341c3a159e9a56d10db5219a849927dfa4fc876
Markus Shepherd <markus.schepke@maas.global>
2020-08-06 15:40:04 +0300

    parse timeout strings

--- board_game_scraper/full_merge.py
+++ board_game_scraper/full_merge.py
@@ -10,12 +10,29 @@ import sys
 from pathlib import Path
 from time import sleep
 
+from pytility import parse_float
 from scrapy.utils.project import get_project_settings
 from yaml import safe_load
 
 LOGGER = logging.getLogger(__name__)
 SETTINGS = get_project_settings()
 BASE_DIR = Path(SETTINGS.get("BASE_DIR") or ".").resolve()
+
+
+def _parse_timeout(timeout):
+    if timeout is None or timeout == "":
+        return None
+
+    timeout_float = parse_float(timeout)
+    if timeout_float is not None:
+        return timeout_float
+
+    try:
+        import pytimeparse
+    except ImportError:
+        return None
+
+    return pytimeparse.parse(timeout)
 
 
 def _docker_container(name):
@@ -58,23 +75,31 @@ def _docker_start(name):
     return False
 
 
-def _docker_stop(name, timeout=120):
+def _docker_stop(name, timeout=None):
     LOGGER.info("Stopping container <%s>", name)
+    if timeout is not None:
+        LOGGER.info("Allowing a timeout of %.1f seconds", timeout)
 
     container = _docker_container(name)
 
     if container is not None:
         try:
-            container.stop(timeout=timeout)
+            if timeout is None:
+                container.stop()
+            else:
+                container.stop(timeout=timeout)
             LOGGER.info("Stopped via Docker library call")
             return True
         except Exception:
             pass
 
     try:
-        subprocess.run(
-            ["docker-compose", "stop", "--timeout", str(timeout), name], check=True
+        args = (
+            ["docker-compose", "stop", name]
+            if timeout is None
+            else ["docker-compose", "stop", "--timeout", str(timeout), name]
         )
+        subprocess.run(args=args, check=True)
         LOGGER.info("Stopped via docker-compose CLI call")
         return True
     except Exception:
@@ -96,13 +121,19 @@ def _docker_compose(path, service):
     return {}
 
 
-def _stop_merge_start(spider, compose_file, cool_down=None):
+def _stop_merge_start(spider, compose_file, timeout=None, cool_down=None):
     LOGGER.info("Stopping, merging, and restarting spider <%s>", spider)
 
     config = _docker_compose(path=compose_file, service=spider)
     container = config.get("container_name")
 
-    _docker_stop(name=container)
+    timeout = _parse_timeout(timeout)
+    if timeout is None:
+        timeout = _parse_timeout(config.get("stop_grace_period"))
+
+    if not _docker_stop(name=container, timeout=timeout):
+        LOGGER.error("Unable to stop <%s>, aborting", container)
+        return False
 
     if cool_down:
         LOGGER.info("Cooling down for %d seconds...", cool_down)
@@ -114,7 +145,7 @@ def _stop_merge_start(spider, compose_file, cool_down=None):
         LOGGER.info("Cooling down for %d seconds...", cool_down)
         sleep(cool_down)
 
-    _docker_start(name=container)
+    return _docker_start(name=container)
 
 
 def _parse_args():
@@ -123,6 +154,7 @@ def _parse_args():
     parser.add_argument(
         "--compose-file", "-c", default=BASE_DIR / "docker-compose.yaml"
     )
+    parser.add_argument("--timeout", "-t", help="")
     parser.add_argument("--cool-down", "-d", type=int, default=60, help="")
     parser.add_argument(
         "--verbose",
@@ -150,7 +182,10 @@ def main():
 
     for spider in args.spiders:
         _stop_merge_start(
-            spider=spider, compose_file=args.compose_file, cool_down=args.cool_down
+            spider=spider,
+            compose_file=args.compose_file,
+            timeout=args.timeout,
+            cool_down=args.cool_down,
         )
 
 
