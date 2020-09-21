f5d617c17eb89cf54b9601a7a7575035590dff8e
Markus Schepke <markus.schepke@wolt.com>
2020-09-21 21:17:35 +0300

    better geeklist scraping

--- board_game_scraper/spiders/bgg_geeklist.py
+++ board_game_scraper/spiders/bgg_geeklist.py
@@ -7,7 +7,7 @@ from scrapy import Spider
 
 from ..items import GameItem
 from ..loaders import GameLoader
-from ..utils import extract_bgg_id
+from ..utils import extract_bgg_id, now
 
 
 class BggGeekListSpider(Spider):
@@ -16,7 +16,7 @@ class BggGeekListSpider(Spider):
     name = "bgg_geeklist"
     allowed_domains = ("boardgamegeek.com",)
     start_urls = (
-        "https://www.boardgamegeek.com/geeklist/17950/bgg-top-50-statistics-01-nov-06-01-dec-06",
+        "https://www.boardgamegeek.com/geeklist/30543/bgg-top-50-statistics-meta-list",
     )
     item_classes = (GameItem,)
 
@@ -39,22 +39,33 @@ class BggGeekListSpider(Spider):
         )
 
         ldr.add_value("name", title.xpath(".//a[2]/text()").extract_first())
-        # ldr.add_xpath("image_url", "thumbnail/@value")
-
-        # ldr.add_value("published_at", response.meta.get("published_at"))
-        # ldr.add_value("published_at", scraped_at)
-        # ldr.add_value("scraped_at", scraped_at)
+        ldr.add_xpath("image_url", ".//a/img[starts-with(@alt, 'Board Game:')]/@src")
 
         return ldr.load_item()
 
-    def parse_item(self, item, response):
+    def parse_geeklist(self, item, response, **kwargs):
+        """Parse geeklist."""
+
+        url = item.css(".geeklist_item_title").xpath(".//a[2]/@href").extract_first()
+
+        if url and "/geeklist/" in url:
+            return response.follow(url=url, callback=self.parse,)
+
+        return None
+
+    def parse_item(self, item, response, **kwargs):
         """Decide on the item type and call corresponding method."""
+
         # TODO decide what type and call corresponding method
+
         rank_text = (
             item.css(".geeklist_item_title").xpath(".//a[1]/text()").extract_first()
         )
         rank = parse_int(rank_text[:-1]) if rank_text else None
-        return self.parse_game(item=item, response=response, rank=rank)
+
+        return self.parse_game(
+            item=item, response=response, rank=rank, **kwargs,
+        ) or self.parse_geeklist(item=item, response=response)
 
     def parse(self, response):
         """
@@ -64,5 +75,7 @@ class BggGeekListSpider(Spider):
         @scrapes TODO
         """
 
+        scraped_at = now()
+
         for item in response.xpath("//*[@data-objecttype = 'listitem']"):
-            yield self.parse_item(item=item, response=response)
+            yield self.parse_item(item=item, response=response, scraped_at=scraped_at)
