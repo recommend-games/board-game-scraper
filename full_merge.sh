c8fb8b22d892efe1ebf57f2b07ad03e74b27f12d
Markus Schepke <markus.schepke@wolt.com>
2021-08-26 10:01:38 +0300

    make spiders in full_merge.sh more flexible

--- full_merge.sh
+++ full_merge.sh
@@ -3,20 +3,19 @@
 set -euo pipefail
 
 BASE_DIR="$(dirname "$(readlink --canonicalize "${BASH_SOURCE[0]}")")"
-LOGS_DIR="$(readlink --canonicalize "${1:-${BASE_DIR}/logs}")"
+LOGS_DIR="$(readlink --canonicalize "${BASE_DIR}/logs")"
 
-echo -e "Merging feeds, writing logs to <${LOGS_DIR}>…\\n"
+if [ $# -eq 0 ] || [[ "${1}" == 'all' ]]; then
+    SPIDERS=('bga' 'bgg_hotness' 'dbpedia' 'luding' 'spielen' 'wikidata' 'bgg')
+else
+    SPIDERS=("$@")
+fi
+
+echo -e "Merging feeds <${SPIDERS[@]}>, writing logs to <${LOGS_DIR}>…\\n"
 
 mkdir --parents "${LOGS_DIR}"
 
 nohup pipenv run \
-    python -m board_game_scraper.full_merge \
-        bga \
-        bgg_hotness \
-        dbpedia \
-        luding \
-        spielen \
-        wikidata \
-        bgg \
+    python -m board_game_scraper.full_merge "${SPIDERS[@]}" \
     >> "${LOGS_DIR}/merge.log" 2>&1 &
 echo -e "Started! Follow logs from <${LOGS_DIR}/merge.log>.\\n"
