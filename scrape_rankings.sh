837f8147fb776176290523eea73cadbd8d89b34c
Markus Schepke <markus.schepke@wolt.com>
2021-10-31 07:09:56 +0200

    added scrape_rankings.sh to remove concurrent processes

---
+++ scrape_rankings.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+python -m board_game_scraper bgg_hotness \
+    --max-sleep-process 60 \
+    --set CLOSESPIDER_TIMEOUT=21600 \
+    --set DONT_RUN_BEFORE_SEC=10800
+python -m board_game_scraper bgg_rankings \
+    --max-sleep-process 60 \
+    --set CLOSESPIDER_TIMEOUT=36000 \
+    --set DONT_RUN_BEFORE_SEC=21600
+python -m board_game_scraper bgg_rankings \
+    --max-sleep-process 60 \
+    --feeds-subdir bgg_rankings_abstract \
+    -a bgg_path=abstracts/browse/boardgame \
+    --set CLOSESPIDER_TIMEOUT=36000 \
+    --set DONT_RUN_BEFORE_SEC=21600
+python -m board_game_scraper bgg_rankings \
+    --max-sleep-process 60 \
+    --feeds-subdir bgg_rankings_children \
+    -a bgg_path=childrensgames/browse/boardgame \
+    --set CLOSESPIDER_TIMEOUT=36000 \
+    --set DONT_RUN_BEFORE_SEC=21600
+python -m board_game_scraper bgg_rankings \
+    --max-sleep-process 60 \
+    --feeds-subdir bgg_rankings_customizable \
+    -a bgg_path=cgs/browse/boardgame \
+    --set CLOSESPIDER_TIMEOUT=36000 \
+    --set DONT_RUN_BEFORE_SEC=21600
+python -m board_game_scraper bgg_rankings \
+    --max-sleep-process 60 \
+    --feeds-subdir bgg_rankings_family \
+    -a bgg_path=familygames/browse/boardgame \
+    --set CLOSESPIDER_TIMEOUT=36000 \
+    --set DONT_RUN_BEFORE_SEC=21600
+python -m board_game_scraper bgg_rankings \
+    --max-sleep-process 60 \
+    --feeds-subdir bgg_rankings_party \
+    -a bgg_path=partygames/browse/boardgame \
+    --set CLOSESPIDER_TIMEOUT=36000 \
+    --set DONT_RUN_BEFORE_SEC=21600
+python -m board_game_scraper bgg_rankings \
+    --max-sleep-process 60 \
+    --feeds-subdir bgg_rankings_strategy \
+    -a bgg_path=strategygames/browse/boardgame \
+    --set CLOSESPIDER_TIMEOUT=36000 \
+    --set DONT_RUN_BEFORE_SEC=21600
+python -m board_game_scraper bgg_rankings \
+    --max-sleep-process 60 \
+    --feeds-subdir bgg_rankings_thematic \
+    -a bgg_path=thematic/browse/boardgame \
+    --set CLOSESPIDER_TIMEOUT=36000 \
+    --set DONT_RUN_BEFORE_SEC=21600
+python -m board_game_scraper bgg_rankings \
+    --max-sleep-process 60 \
+    --feeds-subdir bgg_rankings_war \
+    -a bgg_path=wargames/browse/boardgame \
+    --set CLOSESPIDER_TIMEOUT=36000 \
+    --set DONT_RUN_BEFORE_SEC=21600
+python -m board_game_scraper bgg_geeklist \
+    --max-sleep-process 60 \
+    --feeds-subdir bgg_rankings \
+    --file-tag "-geeklist${SCRAPER_FILE_TAG}" \
+    --set CLOSESPIDER_TIMEOUT=129600 \
+    --set DONT_RUN_BEFORE_SEC=86400
