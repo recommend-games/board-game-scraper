e67bce957491b499bd2213d88fd9de4462db3c20
Markus Schepke <markus.schepke@wolt.com>
2021-10-31 07:36:35 +0200

    make MAX_SLEEP_PROCESS configurable

--- scrape_sites.sh
+++ scrape_sites.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -xo pipefail
+
+MAX_SLEEP_PROCESS="${MAX_SLEEP_PROCESS:-0}"
 
 python -m board_game_scraper bga \
-    --max-sleep-process 60 \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
 python -m board_game_scraper dbpedia \
-    --max-sleep-process 60 \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
 python -m board_game_scraper luding \
-    --max-sleep-process 60 \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
 python -m board_game_scraper spielen \
-    --max-sleep-process 60 \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
 python -m board_game_scraper wikidata \
-    --max-sleep-process 60 \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
