e132adb3e9a76529487c18a007d9bdc4595fff6c
Markus Schepke <markus.schepke@wolt.com>
2021-10-31 07:39:44 +0200

    let's disable BGA spider since it doesn't work

--- scrape_sites.sh
+++ scrape_sites.sh
@@ -4,10 +4,10 @@ set -xo pipefail
 
 MAX_SLEEP_PROCESS="${MAX_SLEEP_PROCESS:-0}"
 
-python -m board_game_scraper bga \
-    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
-    --set CLOSESPIDER_TIMEOUT=36000 \
-    --set DONT_RUN_BEFORE_SEC=21600
+# python -m board_game_scraper bga \
+#     --max-sleep-process "${MAX_SLEEP_PROCESS}" \
+#     --set CLOSESPIDER_TIMEOUT=36000 \
+#     --set DONT_RUN_BEFORE_SEC=21600
 python -m board_game_scraper dbpedia \
     --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --set CLOSESPIDER_TIMEOUT=36000 \
