f808d0d0007ddfa2e6bffb4b24ab239ed85fd8b8
Markus Schepke <markus.schepke@wolt.com>
2021-11-01 09:36:41 +0200

    fix small bug in scrape_rankings.sh

--- scrape_rankings.sh
+++ scrape_rankings.sh
@@ -5,63 +5,64 @@ set -exo pipefail
 MAX_SLEEP_PROCESS="${MAX_SLEEP_PROCESS:-0}"
 
 python -m board_game_scraper bgg_hotness \
-    --max-sleep-process "${MAX_SLEEP_PROCESS:-0}" \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --set CLOSESPIDER_TIMEOUT=21600 \
     --set DONT_RUN_BEFORE_SEC=10800
 python -m board_game_scraper bgg_rankings \
-    --max-sleep-process "${MAX_SLEEP_PROCESS:-0}" \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
 python -m board_game_scraper bgg_rankings \
-    --max-sleep-process "${MAX_SLEEP_PROCESS:-0}" \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --feeds-subdir bgg_rankings_abstract \
     -a bgg_path=abstracts/browse/boardgame \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
 python -m board_game_scraper bgg_rankings \
-    --max-sleep-process "${MAX_SLEEP_PROCESS:-0}" \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --feeds-subdir bgg_rankings_children \
     -a bgg_path=childrensgames/browse/boardgame \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
 python -m board_game_scraper bgg_rankings \
-    --max-sleep-process "${MAX_SLEEP_PROCESS:-0}" \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --feeds-subdir bgg_rankings_customizable \
     -a bgg_path=cgs/browse/boardgame \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
 python -m board_game_scraper bgg_rankings \
-    --max-sleep-process "${MAX_SLEEP_PROCESS:-0}" \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --feeds-subdir bgg_rankings_family \
     -a bgg_path=familygames/browse/boardgame \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
 python -m board_game_scraper bgg_rankings \
-    --max-sleep-process "${MAX_SLEEP_PROCESS:-0}" \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --feeds-subdir bgg_rankings_party \
     -a bgg_path=partygames/browse/boardgame \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
 python -m board_game_scraper bgg_rankings \
-    --max-sleep-process "${MAX_SLEEP_PROCESS:-0}" \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --feeds-subdir bgg_rankings_strategy \
     -a bgg_path=strategygames/browse/boardgame \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
 python -m board_game_scraper bgg_rankings \
-    --max-sleep-process "${MAX_SLEEP_PROCESS:-0}" \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --feeds-subdir bgg_rankings_thematic \
     -a bgg_path=thematic/browse/boardgame \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
 python -m board_game_scraper bgg_rankings \
-    --max-sleep-process "${MAX_SLEEP_PROCESS:-0}" \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --feeds-subdir bgg_rankings_war \
     -a bgg_path=wargames/browse/boardgame \
     --set CLOSESPIDER_TIMEOUT=36000 \
     --set DONT_RUN_BEFORE_SEC=21600
+# TODO -geeklist is interpreted as a cmd argument, needs proper escaping!
 python -m board_game_scraper bgg_geeklist \
-    --max-sleep-process "${MAX_SLEEP_PROCESS:-0}" \
+    --max-sleep-process "${MAX_SLEEP_PROCESS}" \
     --feeds-subdir bgg_rankings \
     --file-tag "-geeklist${SCRAPER_FILE_TAG}" \
     --set CLOSESPIDER_TIMEOUT=129600 \
