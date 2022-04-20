223a1352d2b32c1eda8f6ef64c06c58b3cbf634e
Markus Schepke <markus.schepke@wolt.com>
2022-04-20 09:14:24 +0300

    first proper version of shuffle.sh

--- shuffle.sh
+++ shuffle.sh
@@ -1,6 +1,23 @@
-for SERVICE in $(docker compose ps --services | shuf)
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BASE_DIR="$(dirname "$(readlink --canonicalize "${BASH_SOURCE[0]}")")"
+COMPOSE_FILE="${BASE_DIR}/docker-compose.yaml"
+SLEEP_TIME=600
+
+if [ $# -eq 0 ] || [[ "${1}" == 'all' ]]; then
+    SERVICES=$(docker compose ps --services | shuf)
+else
+    SERVICES=$(shuf --echo "$@")
+fi
+
+echo "Running services <${SERVICES[@]}> from compose file <${COMPOSE_FILE}>…"
+
+for SERVICE in ${SERVICES}
 do
-    echo "${SERVICE}"
-    docker compose up -d "${SERVICE}"
-    sleep 600
+    echo "Starting ${SERVICE}…"
+    docker compose --file "${COMPOSE_FILE}" up --detach "${SERVICE}"
+    echo "Going to sleep for ${SLEEP_TIME} seconds…"
+    sleep "${SLEEP_TIME}s"
 done
