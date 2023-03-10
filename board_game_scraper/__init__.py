8fb713705cbc1a29a4c6d2ec15cec9302124d345
Markus Schepke <markus.schepke@wolt.com>
2023-03-10 11:37:31 +0200

    calm down numba logging

--- board_game_scraper/__init__.py
+++ board_game_scraper/__init__.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
-
-""" monkey-patches """
+"""Configs and monkey-patches."""
 
 import csv
+import logging
 import sys
 
 from scrapy.http import XmlResponse
 from scrapy.responsetypes import responsetypes
 
 from .__version__ import VERSION, __version__
+
+logging.getLogger("numba").setLevel(logging.INFO)
 
 csv.field_size_limit(sys.maxsize)
 
--- board_game_scraper/__version__.py
+++ board_game_scraper/__version__.py
@@ -2,5 +2,5 @@
 
 """Version."""
 
-VERSION = (2, 19, 0)
+VERSION = (2, 19, 1)
 __version__ = ".".join(map(str, VERSION))
