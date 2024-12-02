8129870f1862e926b58851010c6574f11013771b
Markus Schepke <markus.schepke@wolt.com>
2024-12-02 22:10:36 +0200

    Added ISODateTimeJSONEncoder to SparseJsonLinesItemExporter

--- src/board_game_scraper/exporters.py
+++ src/board_game_scraper/exporters.py
@@ -1,11 +1,32 @@
+from datetime import date, datetime, time
 from typing import Any
 
 from itemadapter import ItemAdapter
 from scrapy.exporters import JsonLinesItemExporter
+from scrapy.utils.serialize import ScrapyJSONEncoder
+
+
+class ISODateTimeJSONEncoder(ScrapyJSONEncoder):
+    """Custom JSON encoder for datetime objects."""
+
+    def default(self, o: Any) -> Any:
+        """
+        Serialize the given object to JSON, using ISO date and time formats for
+        date, datetime and time objects.
+        """
+
+        if isinstance(o, (date, datetime, time)):
+            return o.isoformat()
+
+        return super().default(o)
 
 
 class SparseJsonLinesItemExporter(JsonLinesItemExporter):
     """Recursively remove falsey values from items before exporting them."""
+
+    def __init__(self, *args: Any, **kwargs: Any):
+        super().__init__(*args, **kwargs)
+        self.encoder = ISODateTimeJSONEncoder(**self._kwargs)
 
     def remove_falsey_values(self, item: Any) -> None:
         """Recursively remove falsey values from the given item."""
