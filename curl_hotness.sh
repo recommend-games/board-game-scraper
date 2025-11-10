77c6c8d28c7a3464ed485206c003a786bce2b7fd
Markus Schepke <markus.schepke@wolt.com>
2025-11-10 12:09:32 +0200

    Added BGG_API_AUTH_TOKEN to curl_hotness.sh

--- curl_hotness.sh
+++ curl_hotness.sh
@@ -9,7 +9,16 @@ DEFAULT_DIR="${BASE_DIR}/feeds/bgg_hotness/raw"
 DEST_DIR="${1:-${DEFAULT_DIR}}"
 DEST_FILE="${DEST_DIR}/$(date -u '+%Y-%m-%dT%H-%M-%S').xml"
 
+BGG_API_AUTH_TOKEN="${BGG_API_AUTH_TOKEN:-""}"
+if [[ -z "${BGG_API_AUTH_TOKEN}" ]]; then
+  echo "Warning: BGG_API_AUTH_TOKEN is required for authenticated requests but is not set."
+  exit 1
+fi
+
 echo "Saving latest BGG hotness from <${URL}> to ${DEST_FILE}…"
 mkdir -p "${DEST_DIR}"
-curl "${URL}" --compressed --output "${DEST_FILE}"
+curl "${URL}" \
+    --compressed \
+    --header "Authorization: Bearer ${BGG_API_AUTH_TOKEN}" \
+    --output "${DEST_FILE}"
 echo "Done."
